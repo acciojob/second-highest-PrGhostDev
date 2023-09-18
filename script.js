10ca1a7e8b88ec65f2cf4e715a28751e0e52d7b9
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-09-18 21:53:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) {
+        return -Infinity;
+    }
+    
+    // Sort the array in descending order
+    arr.sort(function(a, b) {
+        return b - a;
+    });
+    
+    // Find and return the second element
+    return arr[1];
+}
+
+function Main() {
+    var n = prompt("Enter the number of elements");
+    var arr = [];
+    for (var i = 0; i < n; i++) {
+        arr[i] = parseInt(prompt("Enter element " + (i+1))); // Parse input as integers
+    }
+    alert(secondHighest(arr));
+}
+Main();
